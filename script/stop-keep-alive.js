279ef509c37d83da19d807a44caa54231fe4ba93
hqrin <mevcastillo2017@gmail.com>
2026-07-06 02:49:08 +0200

    feat: refactor stop-keep-alive script to use control directory and state management

--- script/stop-keep-alive.js
+++ script/stop-keep-alive.js
@@ -2,17 +2,28 @@
 const fs = require('fs');
 const path = require('path');
 
-const stopFile = path.join(__dirname, 'stop-keep-alive');
-const pidFile = path.join(__dirname, 'keep-alive.pid');
+const controlDir = path.join(__dirname, '.control');
+const markerFile = path.join(controlDir, 'stop-marker');
+const stateFile = path.join(controlDir, 'state.json');
 
-fs.writeFileSync(stopFile, '1', 'utf8');
+if (!fs.existsSync(controlDir)) {
+  fs.mkdirSync(controlDir, { recursive: true });
+}
+
+fs.writeFileSync(markerFile, '1', 'utf8');
 console.log('[keep-alive] Se ha solicitado detener el servicio.');
 
-if (fs.existsSync(pidFile)) {
-  const pid = Number(fs.readFileSync(pidFile, 'utf8'));
+if (fs.existsSync(stateFile)) {
   try {
-    process.kill(pid);
+    const state = JSON.parse(fs.readFileSync(stateFile, 'utf8'));
+    if (state && state.pid) {
+      try {
+        process.kill(state.pid, 'SIGTERM');
+      } catch (error) {
+        // ignore
+      }
+    }
   } catch (error) {
-    console.log('[keep-alive] El proceso ya no está en ejecución.');
+    // ignore
   }
 }
